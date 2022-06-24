2de7e37684ec843a5ee3b12dee1bb65eecd691a8
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-06-24 11:52:47 +0800

    Django Media Serv

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='django_media_serv',
+      version='1.1',
+      description='Django Media Serv',
+      url='https://github.com/dbca-wa/django-media-serv',
+      author='Department of Parks and Wildlife',
+      author_email='asi@dbca.wa.gov.au',
+      license='BSD',
+      packages=['django-media-serv',
+                ],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
+
